1bd955fdea7dd49d87feaf2f9bb87e3a60fb773c
skad <skadprog@gmail.com>
2017-09-12 14:34:22 +0200

    Update and clean
      - using ls for better file finding
      - add {} into the file in case of no data

--- buildSexbound.sh
+++ buildSexbound.sh
@@ -7,7 +7,7 @@ DATA_DIR="$BASE/mods/SexboundAPI_Data"
 DATA_BUILD_DIR="$DATA_DIR/build"
 PLAYER_DATA="$BASE/storage/player"
 PLAYER_IDENTITIES_FILE="$DATA_DIR/playeridentities.json"
-
+NB_FILES=0
 echo $DATA_DIR
 echo $DATA_BUILD_DIR
 echo $PLAYER_DATA
@@ -17,11 +17,14 @@ mkdir -p $DATA_DIR
 mkdir -p $DATA_BUILD_DIR
 
 #clean identities file
-echo "" > $PLAYER_IDENTITIES_FILE
-for file in $PLAYER_DATA/*.player
+rm $PLAYER_IDENTITIES_FILE
+
+for file in $(ls $PLAYER_DATA | grep \\.player\$)
 do
-	file=${file##*/}
-  PLAYER_DATA_FILE="${PLAYER_DATA}/${file}"
+	NB_FILES=$NB_FILES+1
+	#file=${file##*/}
+	echo $file
+	PLAYER_DATA_FILE="${PLAYER_DATA}/${file}"
 	PLAYER_DATA_DUMP_JSON="${DATA_BUILD_DIR}/${file}.json"
 
 	$("$BASE/linux/dump_versioned_json" $PLAYER_DATA_FILE $PLAYER_DATA_DUMP_JSON)
@@ -29,8 +32,11 @@ do
 	$(cat $PLAYER_DATA_DUMP_JSON | jq ' .content | {(.uuid) : .identity} ' >> $PLAYER_IDENTITIES_FILE)
 	echo "," >> $PLAYER_IDENTITIES_FILE
 done
-
-#remove last coma
-sed -i '$ s/.$//' $PLAYER_IDENTITIES_FILE
-
+if [ "$NB_FILES" == 0 2> /dev/null ]
+then
+	echo "{}" > $PLAYER_IDENTITIES_FILE
+else
+	#remove last coma
+	sed -i '$ s/.$//' $PLAYER_IDENTITIES_FILE
+fi
 echo "Build finished... Have a nice game"
